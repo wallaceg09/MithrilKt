2142a4c181bbea9dceb6e3d56ba124307663c76a
Glen Wallace <wallaceglenw@gmail.com>
2017-04-27 23:09:09 -0500

    Remove JsObject inheritance from HtmlAttribute

--- src/main/kotlin/com/wallace/glen/js/mithril/HtmlAttribute.kt
+++ src/main/kotlin/com/wallace/glen/js/mithril/HtmlAttribute.kt
@@ -1,17 +1,7 @@
 package com.wallace.glen.js.mithril
 
-import com.wallace.glen.js.JsObject
-
 /**
  * Created by Glen on 4/26/2017.
  */
-class HtmlAttribute (var `class` : String? = null, var id : String? = null): JsObject {
-    override fun toJsObject() : dynamic {
-        val jsObject : dynamic = object {}
-
-        if (this.`class` != null) jsObject["class"] = `class`
-        if (this.id != null) jsObject["id"] = id
-
-        return jsObject
-    }
+class HtmlAttribute (var `class` : String = "", var id : String = "") {
 }
