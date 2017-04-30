965cbb44940a922b5c0242a42f64715664b94f58
Glen Wallace <wallaceglenw@gmail.com>
2017-04-29 21:10:22 -0500

    Add interfaces for the various lifetime hooks

--- wrapper/src/main/kotlin/com/wallace/glen/js/mithril/VNode.kt
+++ wrapper/src/main/kotlin/com/wallace/glen/js/mithril/VNode.kt
@@ -5,7 +5,8 @@ import org.w3c.dom.Element
 /**
  * Created by Glen on 4/27/2017.
  */
-external class VNode {
+external class VNode
+{
     var tag: dynamic
     var key: String?
     var attrs: Any?
@@ -17,4 +18,11 @@ external class VNode {
     var _state: dynamic
     var events: Any?
     var instance: Any?
+
+    val oninit: Function<*>
+    val oncreate: Function<*>
+    val onremove: Function<*>
+    val onupdate: Function<*>
+    val onbeforeremove: Function<*>
+    val onbeforeupdate: Function<*>
 }
---
+++ wrapper/src/main/kotlin/com/wallace/glen/js/mithril/lifetime/BeforeUpdateable.kt
@@ -0,0 +1,8 @@
+package com.wallace.glen.js.mithril.lifetime
+
+/**
+ * Created by Glen on 4/29/2017.
+ */
+interface BeforeUpdateable {
+    val onbeforeupdate: Function<*>
+}
